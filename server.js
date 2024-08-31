e6b7f56fcc2b25449c91d1784d8cc74ea0080fb5
emirkugic <emir.kugic@hotmail.com>
2024-08-31 04:38:58 +0200

    REFACTOR

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const cors = require("cors"); // <-- Add this line
+const cors = require("cors");
 require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
